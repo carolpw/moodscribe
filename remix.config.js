4eb102766d4dc870d510a4919c26e819d49fe5b6
carolpw <lopezolea.c@gmail.com>
2025-07-23 11:43:07 +0200

    Delete remix.config.js

--- remix.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-    serverBuildTarget: "cloudflare-pages",
-    server: "./functions/_worker.js",
-    publicPath: "/build/",
-    assetsBuildDirectory: "public/build",
-  };
